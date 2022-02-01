4701263057d14067c511eab82c26cb846ca4c792
Michael <michael.daniels1013@gmail.com>
2022-02-01 00:19:49 -0600

    commit tree c95eab1ccaff0a2f6897fbde4f1cdc23a2a3c6af

--- index.js
+++ index.js
@@ -1,11 +1,51 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  let response;
+  if (distance <= 400){
+    response = `This one is on me!`;
+    } else if (distance > 2500){
+      response = `No can do.`;
+    } else if (distance > 2000){
+    response = `I will gladly take your thirty bucks.`;
+    } 
+    return response;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+
+
+
+
+
+function ternaryCheckCity(city){
+  const cityMessage = city === 'NYC' ? "Ok, sounds good." : "No go.";
+  return cityMessage;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+
+
+
+
+
+
+
+
+
+
+
+
+function switchOnCharmFromTip(tip){
+  let response;
+  switch (tip) {
+    case 'generous':
+      response = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      response = 'Thank you.';
+      break;
+    default:
+      response = 'Bye.';
+      break;
+  }
+  return response;
 }
